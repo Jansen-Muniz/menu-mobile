a24902e6090ee542aa7164b9b36a6d42b07a4193
Jansen Muniz <jansen.muniz1@gmail.com>
2022-09-13 18:35:46 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,45 @@
+const nav = document.querySelector('.nav')
+const btnMenu = document.querySelector('.btn-menu')
+const menu = document.querySelector('.menu')
+
+function handleButtonClick(event) {
+  if (event.type === 'touchstart') event.preventDefault()
+
+  event.stopPropagation()
+  nav.classList.toggle('active')
+  handleClickOutside(menu, () => {
+    nav.classList.remove('active')
+    setAria()
+  })
+  setAria()
+}
+
+function handleClickOutside(targetElement, callback) {
+  const html = document.documentElement
+  function handleHTMLCLick(event) {
+    if (!targetElement.contains(event.target)) {
+      targetElement.removeAttribute('data-target')
+      html.removeEventListener('click', handleHTMLCLick)
+      html.removeEventListener('touchstart', handleHTMLCLick)
+      callback()
+    }
+  }
+  if (!targetElement.hasAttribute('data-target')) {
+    html.addEventListener('click', handleHTMLCLick)
+    html.addEventListener('touchstart', handleHTMLCLick)
+    targetElement.setAttribute('data-target', '')
+  }
+}
+
+function setAria() {
+  const isActive = nav.classList.contains('active')
+  btnMenu.setAttribute('aria-expanded', isActive)
+  if (isActive) {
+    btnMenu.setAttribute('aria-label', 'Fechar Menu')
+  } else {
+    btnMenu.setAttribute('aria-label', 'Abrir Menu')
+  }
+}
+
+btnMenu.addEventListener('click', handleButtonClick)
+btnMenu.addEventListener('touchstart', handleButtonClick)
